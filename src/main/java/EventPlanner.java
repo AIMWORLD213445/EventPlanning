080195b5ee075d4e0aa6f5e72fa4f7224d45076f
Brian Dutz <bgdutz@gmail.com>
2016-09-09 10:26:06 -0700

    add basic testing for an instance of my EventPlanner class and pass it through BDD

---
+++ src/main/java/EventPlanner.java
@@ -0,0 +1,36 @@
+public class EventPlanner {
+  // private int mGuestNumber;
+  // private String mFood;
+  // private String mBverages;
+  // private String mEntertainment;
+
+  // public EventPlanner(int guestNumber, String food, String beverages, String entertainment) {
+  //   mGuestNumber = guestNumber;
+  //   mFood = food;
+  //   mBeverages = beverages;
+  //   mEntertainment = entertainment;
+  // }
+  //
+  // public int getGuestNumber() {
+  //   return mGuestNumber;
+  // }
+  // public String getFood() {
+  //   return mFood;
+  // }
+  // public String getBeverages() {
+  //   return mBeverages;
+  // }
+  // public String getEntertainment() {
+  //   return mEntertainment;
+  // }
+
+
+
+
+
+  // public Integer calculateBill(String service){
+  //   if(service.equals("Chicken Dinner")){
+  //
+  //   }
+  // }
+}
--- src/test/java/EventPlannerTest.java
+++ src/test/java/EventPlannerTest.java
@@ -2,6 +2,17 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class EventPlannerTest {
+
   @Test
+  public void calculateBill_returnBillForAnyService_500(){
+    EventPlanner testEventPlanner = new EventPlanner();
+    assertEquals(true, testEventPlanner instanceof EventPlanner);
+  }
+  // @Test
+  // public void calculateBill_returnBillForAnyService_500(){
+  //   EventPlanner testEventPlanner = new EventPlanner();
+  //   Integer expected = 500;
+  //   assertEquals(expected, testEventPlanner.calculateBill("Chicken Dinner"));
+  // }
 
 }
